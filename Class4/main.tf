7e007c6cef7a112de43720472ff7604c3e70ccf6
nkaken <nkakenov@gmail.com>
2024-10-30 01:20:53 +0000

    vcp files

--- Class4/main.tf
+++ Class4/main.tf
@@ -1,3 +1,18 @@
 resource "aws_vpc" "main" {
-    cidr_block = "10.0.0.0/16"
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+}
+
+resource "aws_subnet" "main1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+}
+
+resource "aws_subnet" "main2" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.3.0/24"
 }
