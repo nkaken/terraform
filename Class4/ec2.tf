b60c6d447bd252379f8f7e7595dc098352122bdc
nkaken <nkakenov@gmail.com>
2024-10-30 14:55:56 +0000

    vcp files

---
+++ Class4/ec2.tf
@@ -0,0 +1,31 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = "t2.micro"
+  associate_public_ip_address = true
+  key_name                    = aws_key_pair.deployer.key_name
+  vpc_security_group_ids      = [aws_security_group.allow_tls.id]
+  subnet_id                   = aws_subnet.main2.id
+  user_data                   = file("apache.sh")
+
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "keynm"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
