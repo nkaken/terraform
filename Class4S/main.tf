f72061dcc3e524b6a2cb7783c227c06f9efcb712
nkaken <nkakenov@gmail.com>
2024-11-18 05:53:21 +0000

    provision

--- Class4S/main.tf
+++ Class4S/main.tf
@@ -1,29 +1,87 @@
-# resource "aws_instance" "cluster" {
-#   count         = 1
-#   ami           = "ami-0dcc1e21636832c5d"
-#   instance_type = "m5.large"
+data "aws_ami" "ubuntu" {
+  most_recent = true
 
-#   # ...
-# }
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  availability_zone = "us-east-2a"
+  key_name = aws_key_pair.deployer.key_name
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "deplyr-key"
+  public_key = file("~/.ssh/id_rsa.pub")
+
+}
+
+output ec2 {
+  value       = aws_instance.web.public_ip
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+    ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+    description      = "TLS from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    }
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+} 
+}
 
 resource "null_resource" "cluster" {
-  # Changes to any instance of the cluster requires re-provisioning
-  # triggers = {
-  #   cluster_instance_ids = join(",", aws_instance.cluster[*].id)
-  # }
+  triggers = {
+    always_run = "${timestamp()}"
+  }
 
-  # Bootstrap script can run on any instance of the cluster
-  # So we just choose the first in this case
-  # connection {
-  #   host = element(aws_instance.cluster[*].public_ip, 0)
-  # }
+  connection {
+    host = element(aws_instance.web[*].public_ip, 0)
+    type = "ssh"
+    user = "ubuntu"
+    private_key = file("~/.ssh/id_rsa")
+  }
 
-  provisioner "local-exec" {
-    # Bootstrap script called with private_ip of each node in the cluster
-    # inline = [
-    #   "bootstrap-cluster.sh ${join(" ",
-    #   aws_instance.cluster[*].private_ip)}",
-    # ]
-    command = "mkdir kaizen && touch testfile"
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt update -y",
+      "sudo apt install apache2 -y"
+    ]
   }
 }
