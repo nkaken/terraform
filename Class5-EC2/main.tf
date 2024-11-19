dc9aaa626d4bd3c3b822740a75aee3ea3b480bfc
EC2 Default User <ec2-user@ip-172-31-37-251.ec2.internal>
2024-11-19 06:24:59 +0000

    Class 5 ec2

--- Class5-EC2/main.tf
+++ Class5-EC2/main.tf
@@ -1,21 +1,13 @@
 resource "aws_instance" "web" {
-  ami           = "ami-012967cc5a8c9f891"
+  ami           = "ami-000a6d48d55ddd8ac"
   instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.allow_tlss.id]
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
   
 }
 
-resource "aws_security_group" "allow_tlss" {
-  name        = "allow_tlss"
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
   description = "Allow TLS inbound traffic"
-  
-  ingress {
-    description      = "TLS from VPC"
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    }
 
   ingress {
     description      = "TLS from VPC"
@@ -24,6 +16,7 @@ resource "aws_security_group" "allow_tlss" {
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
     }
+
   ingress {
     description      = "TLS from VPC"
     from_port        = 22
--- Class5/packer.pkr.hcl
+++ Class5/packer.pkr.hcl
@@ -10,27 +10,19 @@ packer {
 source "amazon-ebs" "example" {
   ami_name      = "golden-imag-{{timestamp}}"
   instance_type = "t2.micro"
-  region = "us-east-1"
-  source_ami = "ami-012967cc5a8c9f891"
+  region = "us-east-2"
+  source_ami = "ami-0942ecd5d85baa812"
   ssh_username = "ec2-user"
-  ssh_keypair_name = "packer"
+  ssh_keypair_name = "packer-key"
   ssh_private_key_file = "~/.ssh/id_rsa"
   
   run_tags = {
     Name = "golden imag"
   }
   ami_regions        = ["us-east-1", "us-east-2"]
-#   source_ami_filter {
-#     filters = {
-#       name                = "ubuntu/images/*ubuntu-jammy-22.04-amd64-server-*"
-#       root-device-type    = "ebs"
-#       virtualization-type = "hvm"
-#     }
-#     most_recent = true
-#     owners      = ["099720109477"]
-#   }
-  
 }
+  
+
 
 build {
   sources = ["source.amazon-ebs.example"]
